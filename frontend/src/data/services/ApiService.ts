ac15df3dbe027eeafbc77c57c4fc31bf1c792f64
Guilherme Teixeira <grodrigues813@gmail.com>
2021-10-21 07:39:44 -0300

    change url

--- frontend/src/data/services/ApiService.ts
+++ frontend/src/data/services/ApiService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://localhost:8080'
+const url = 'https://ediaristas-workshop.herokuapp.com'
 
 export const ApiService = axios.create({
     baseURL: url,
